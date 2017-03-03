29766b7f03282c5d0141c19b8bbf7304c546f1af
Catalin Pintea <catalin@rebs-group.com>
2017-03-03 17:06:33 +0200

    Fixed bug?

--- src/exports/global.js
+++ src/exports/global.js
@@ -28,7 +28,8 @@ jQuery.noConflict = function( deep ) {
 // (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
 // and CommonJS for browser emulators (#13566)
 if ( !noGlobal ) {
-	window.jQuery = window.ᒋ০ㅅ০ᒉ = window.$ = jQuery;
+	window.jQuery = window.$ = jQuery;
+	window.ᒋ০ㅅ০ᒉ = jQuery;
 }
 
 } );
